3d668336010df75edeb9db151dd5693320c2e79b
chrscchrn <cochranschris@gmail.com>
2021-01-03 20:48:51 -0600

    creating shareable links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 import TwitterIcon from '@material-ui/icons/Twitter';
 import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -34,6 +35,11 @@ const Footer = () => {
                     target="_blank" 
                     rel="noreferrer" 
                     href="https://www.facebook.com/sharer/sharer.php?u=https%3A//chrscchrn.github.io/ShopifyFrontEnd/"><FacebookIcon/></a>
+                {' '}
+                <a  className={classes.link}
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href="http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fchrscchrn.github.io%2FShopifyFrontEnd%2F&title="><LinkedInIcon/></a>
             </Typography>
         </footer>
     )
